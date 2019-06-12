947723cbb027b330fc3ddc7f36f577508aff8f44
Brandon Toone <brandon@rdlk.co>
2019-06-12 08:53:47 -0600

    Add tests using spec syntax

--- exercises/ruby/word_count/word_count_test.rb
+++ exercises/ruby/word_count/word_count_test.rb
@@ -33,3 +33,29 @@ class WordCountTest < Minitest::Test
   end
 end
 
+## Same tests reimplemented using the spec aka assertion syntax
+#
+class WordCountSpec < Minitest::Spec
+  def setup
+    @words = %w{ 
+      Take me back 
+      back to the stack 
+      back to the strat with the lightening stap 
+    }
+  end
+
+  describe WordCount do
+    describe "#count" do
+      it "must return a Hash" do
+        result = WordCount.count
+        result.must_be_kind_of Hash
+      end
+
+      it "counts word occurances in a sentence" do
+        result = WordCount.count(@words)
+        result['the'].must_equal 3
+      end
+    end
+  end
+end
+
